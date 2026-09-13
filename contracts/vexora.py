171552aee0e4bdcf8077aa0c5b9c13a1882807e0
chimdi-hash <unaezechimdi@gmail.com>
2026-09-13 11:54:08 +0100

    fix: cast Explorer string inputs to integers in constructor

--- contracts/vexora.py
+++ contracts/vexora.py
@@ -742,10 +742,10 @@ class Vexora(gl.Contract):
         next_assessment_id: int = 1,
         next_vexora_id: int = 1,
     ):
-        self.next_domain_id = u256(next_domain_id)
-        self.next_policy_id = u256(next_policy_id)
-        self.next_assessment_id = u256(next_assessment_id)
-        self.next_vexora_id = u256(next_vexora_id)
+        self.next_domain_id = u256(int(next_domain_id))
+        self.next_policy_id = u256(int(next_policy_id))
+        self.next_assessment_id = u256(int(next_assessment_id))
+        self.next_vexora_id = u256(int(next_vexora_id))
 
     def _require_policy(self, policy_id: u256) -> Policy:
         value = self.policies.get(policy_id)
